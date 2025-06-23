4011e4bd5e585fe0cb1350d25e1802d4b3c5b447
Raphael Berger <raphaelmb@gmail.com>
2025-06-23 14:55:47 -0300

    add validation

--- src/main/java/com/raphaelmb/store/controllers/UserController.java
+++ src/main/java/com/raphaelmb/store/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.raphaelmb.store.dtos.UpdateUserRequest;
 import com.raphaelmb.store.dtos.UserDto;
 import com.raphaelmb.store.mappers.UserMapper;
 import com.raphaelmb.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -38,7 +40,9 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody RegisterUserRequest request, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterUserRequest request, UriComponentsBuilder uriBuilder) {
+        if (userRepository.existsByEmail(request.getEmail())) return ResponseEntity.badRequest().body(Map.of("email", "Email is already registered"));
+
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 
--- src/main/java/com/raphaelmb/store/repositories/UserRepository.java
+++ src/main/java/com/raphaelmb/store/repositories/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.raphaelmb.store.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
