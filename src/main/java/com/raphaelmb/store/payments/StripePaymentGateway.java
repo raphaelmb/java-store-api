4df4ee2b8c8c04c2210388fc41979d8e168190a0
Raphael Berger <raphaelmb@gmail.com>
2025-09-30 13:48:45 -0300

    add profiles

--- src/main/java/com/raphaelmb/store/payments/StripePaymentGateway.java
+++ src/main/java/com/raphaelmb/store/payments/StripePaymentGateway.java
@@ -31,7 +31,7 @@ public class StripePaymentGateway implements PaymentGateway{
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntent(order));
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
@@ -73,6 +73,12 @@ public class StripePaymentGateway implements PaymentGateway{
         );
         var paymentIntent = (PaymentIntent) stripeObject;
         return Long.valueOf(paymentIntent.getMetadata().get("order_id"));
+    }
+
+    private SessionCreateParams.PaymentIntentData createPaymentIntent(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", order.getId().toString())
+                .build();
     }
 
     private SessionCreateParams.LineItem createLineItem(OrderItem item) {
